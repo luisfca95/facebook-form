44952b77954f13c17b9258e8eb9b60bcebf7ce64
Luis <luisfca@outlook.pt>
2023-12-19 21:49:23 +0000

    Finalizado funcionalidade de escolha de género com ativação de campo escondido

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const genderAddons = document.getElementById("gender-addon");
+const genderRadios = document.querySelectorAll(".gender-selection fieldset input");
 const selects = document.querySelectorAll("form select");
 
 for(let i = 1; i <= 31; i++) {
@@ -10,4 +12,14 @@ for(let i = 2023; i >= 1905; i--) {
     const option = document.createElement("option");
 
     selects[2].appendChild(option).textContent = i;
+}
+
+for(let radio of genderRadios) {
+    radio.addEventListener("change", function() {
+        genderAddons.classList.add("hide");
+
+        if(radio.value === "P") {
+            genderAddons.classList.remove("hide");       
+        }
+    });
 }
